b9fa715e69e67a46f1d52e96edbe67a1036ec968
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-06-30 19:53:34 -0700

    Correct js added

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let now = new Date();
 let h2 = document.querySelector("h2");
 
-let date = now.getDate();
 let hours = now.getHours();
-let year = now.getFullYear();
+
 let minutes = now.getMinutes();
 
 let days = [
@@ -19,31 +18,39 @@ let day = days[now.getDay()];
 
 h2.innerHTML = `${day} ${hours}:${minutes} `;
 
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#name").innerHTML = response.data.name;
+  document.querySelector("#tempature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#hum").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
 function search(event) {
   event.preventDefault();
-  let input = document.querySelector("#search-text-input");
-
-  let h3 = document.querySelector("h3");
-  if (input.value) {
-    h3.innerHTML = `The weather in ${input.value} is:`;
-  } else {
-    h3.innerHTML = null;
-    alert("Please type a city");
-  }
+  let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
+  let city = document.querySelector("#search-city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
-let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
-let city = "merced";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+function showPosition(position) {
+  let h5 = document.querySelector("h5");
+  h5.innerHTML = `Your latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude} `;
 
-function showTempature(response) {
-  console.log(response.data);
-  let tempature = Math.round(response.data.main.temp);
-  let tempatureElement = document.querySelector("#tempature");
-  tempatureElement.innerHTML = `${tempature}'F`;
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-axios.get(`${apiURL}&appid=${apiKey}`).then(showTempature);
+let button = document.querySelector("#button");
+button.addEventListener("click", getCurrentPosition);
